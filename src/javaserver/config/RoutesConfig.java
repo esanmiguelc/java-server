8bbac0b88f67e818a739f648f20e81f5d4cdc040
esanmiguelc <esanmiguel@gmail.com>
2015-04-14 10:16:34 -0500

    updating routes; updating cob_spec

--- src/javaserver/config/RoutesConfig.java
+++ src/javaserver/config/RoutesConfig.java
@@ -5,7 +5,7 @@ import javaserver.Routes.RoutesRegistrar;
 public class RoutesConfig {
     public static void seedRoutes() {
         RoutesRegistrar.getInstance().registerRoute("/",false, true, "GET", "OPTIONS");
-        RoutesRegistrar.getInstance().registerRoute("/form", false, "GET", "POST", "PUT");
+        RoutesRegistrar.getInstance().registerRoute("/form", false, "GET", "POST", "PUT", "OPTIONS", "DELETE");
         RoutesRegistrar.getInstance().registerRoute("/logs", true, "GET");
         RoutesRegistrar.getInstance().registerRoute("/method_options", false, "GET","HEAD","POST","OPTIONS","PUT");
     }
