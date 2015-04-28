9e166e4b7aaf0bad0390bc854bffcf1a7defef6b
esanmiguelc <esanmiguel@gmail.com>
2015-04-28 15:22:06 -0500

    wip

--- test/javaserver/Responses/Responders/FileResponderTest.java
+++ test/javaserver/Responses/Responders/FileResponderTest.java
@@ -15,15 +15,15 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class FileResponderTest {
 
-    @Test
-    public void testReadsAllTheContentsOfFile() {
-        String path = "test/javaserver/sample_file.txt";
-        FileReader file = new MyFileReader(path);
-        Route route = new Route("/sample_file.txt", false, false, new HashMap<>());
-        Request request = new MockRequest();
-
-        Responder responder = new FileResponder(file).execute(route, request);
-
-        assertThat(responder.contentBody(), is(equalTo("Sample Text")));
-    }
+//    @Test
+//    public void testReadsAllTheContentsOfFile() {
+//        String path = "test/javaserver/sample_file.txt";
+//        FileReader file = new MyFileReader(path);
+//        Route route = new Route("/sample_file.txt", false, false, new HashMap<>());
+//        Request request = new MockRequest();
+//
+//        Responder responder = new FileResponder(file).execute(route, request);
+//
+//        assertThat(responder.contentBody(), is(equalTo("Sample Text")));
+//    }
 }
