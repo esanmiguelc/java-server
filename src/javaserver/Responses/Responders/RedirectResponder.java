14b4fbbfcd6e763593d24809f03cc4b03598f8cf
esanmiguelc <esanmiguel@gmail.com>
2015-05-12 13:31:02 -0500

    changing redirect to use the relative path

--- src/javaserver/Responses/Responders/RedirectResponder.java
+++ src/javaserver/Responses/Responders/RedirectResponder.java
@@ -24,7 +24,7 @@ public class RedirectResponder implements Responder {
 
     @Override
     public List<String> additionalHeaders() {
-        return Arrays.asList("Location: http://localhost:5000/");
+        return Arrays.asList("Location: /");
     }
 
     @Override
